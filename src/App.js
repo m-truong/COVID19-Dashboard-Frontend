d06db66f1f1e42559e43eb9622ffc72fc9e30c63
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-24 22:19:36 -0500

    Added Material-UI dependency and added useEffect hook
    added display flex to space out header elements; created dropdown field to select countries; added useEffect hook to fetch from COVID-19 API endpoint and parse JSON into list of countries

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
 import './App.css';
+import axios from 'axios'
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useState, useEffect, useContext } from 'react'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  // async -> sends request, waits 4 it to complete, parses the data
+  useEffect(() => {
+    const fetchCountryList = async () => {
+      try {
+        const response = await axios.get("https://disease.sh/v3/covid-19/countries")
+        // auto parses JSON
+        console.log(response)
+        // get response.data Array[221]
+        const countriesClean = response.data.map((countryObj) => (
+          {
+            name: countryObj.country,
+            abbrev: countryObj.countryInfo.iso2, // USA, FR
+          }
+        ));
+        // changes state
+        setCountries(countriesClean)
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    // Invokes async fetch 
+    fetchCountryList()
+    // Runs once when component loads
+  }, [])
+
   return (
-    <div className="App">
+    <div className="app">
 
-      {/* Header */}
-      {/* Title + Select Input dropdown field */}
+      {/* Title + Country dropdown  */}
 
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
+      <div className="app__header">
+        <h1>COVID-19 LIVE-MAP</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="select_form">
+            {/* Loop through all the countries and show a drop down list of all the options */}
+            {
+              countries.map((country, idx) => (
+                <MenuItem key={idx} value={country.abbrev}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
 
-      {/* Live COVID-19 Map */}
+      {/* CasesCard */}
+      {/* RecoveredCard */}
+      {/* DeathsCard */}
 
-      {/* Country Cases Table */}
-      {/* Chart.js COVID Daily Cases Line-Graph */}
+      {/* COVID-19 World Map */}
+
+      {/* Countries Most Cases Chart  */}
+      {/* Rising Daily Cases Line-Graph Chart.js */}
 
     </div>
   );
