9ddd25741a2eb970f0f0095bdf2eecd0b602a963
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-26 13:49:01 -0500

    created WorldMap.css and successfully rendered MapContainer using react-leaflet
    added height css to WorldMap.css, and created MapContainer and TileLayer components; interactable WorldMap successfully displays on page

--- src/Components/WorldMap.js
+++ src/Components/WorldMap.js
@@ -1,12 +1,28 @@
-import Box from "@material-ui/core"
-import { Map as GlobalMap, TileLayer } from "react-leaflet"
+import "../Styles/WorldMap.css"
+import { Box } from "@material-ui/core"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
 function WorldMap() {
     return (
         <Box className="map">
-            <GlobalMap>
-                <TileLayer></TileLayer>
-            </GlobalMap>
+            <MapContainer
+                // Needs height styling in order for MapContainer to appear
+                className="map-container--styles"
+                center={[51.0, 19.0]}
+                zoom={4}
+                maxZoom={18}
+                scrollWheelZoom={false}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </Box>
     )
 }
