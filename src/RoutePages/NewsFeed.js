bc0cc7c200e8213c4408c193274b4f55d51bc15e
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-31 20:12:59 -0500

    commiting .eslintcache changes

--- src/RoutePages/NewsFeed.js
+++ src/RoutePages/NewsFeed.js
@@ -17,16 +17,18 @@ export default function NewsFeed() {
     const [tweetsData, setTweetsData] = useState([])
 
     const token = "AAAAAAAAAAAAAAAAAAAAAPw1MQEAAAAA%2BnKaVgyRfG4TfnfvC6Ss8boCUow%3Dj9CVdyRgXy52MbBgLW47gdxRtlF875buXqTHIiGBsq7FFMVRrb"
+
     const getTweetsData = async () => {
         try {
-            const responseTweetData = await fetch("https://api.twitter.com/1.1/search/tweets.json?q=covid&result_type=recent&count=5&lang=en", {
+            const responseTweetData = await fetch("https://api.twitter.com/1.1/search/tweets.json?q=covid&result_type=popular&count=5&lang=en", {
                 mode: 'no-cors',
                 headers: {
                     "Content-Type": "application/json",
-                    "method": "get",
-                    'Authorization': `Bearer ${token}`,
+                    "method": "GET",
+                    'Authorization': `bearer ${token}`,
                 }
             })
+            console.log(responseTweetData)
             const responseJSON = await responseTweetData.json();
             console.log(responseJSON)
         } catch (e) {
@@ -86,7 +88,8 @@ export default function NewsFeed() {
                 </Col>
                 <Col md={4}>
                     <h1>Twitter Feed</h1>
-                    <a class="twitter-timeline" data-lang="en" data-theme="dark" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+                    {/* <a class="twitter-timeline" data-lang="en" data-theme="dark" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> */}
                 </Col>
                 <Col md={4}>
                     <DataCard title="Vaccines Administered" stat={totalVaccines}></DataCard>
